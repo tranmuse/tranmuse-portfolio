18bfb5e74498878a6446ba2d37918b1a8e68a031
TranMuse <tranmuse@gmail.com>
2024-11-05 04:56:26 -0800

    update information

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "TranMuse's Space",
+    template: "%s | TranMuse's Space",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Co-founder of Break Project and a curosity mind",
   openGraph: {
-    title: "chronark.com",
+    title: "tranmuse.com",
     description:
       "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+    url: "https://tranmuse.com",
+    siteName: "tranmuse.com",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "https://tranmuse.com/og.png",
         width: 1920,
         height: 1080,
       },
--- app/page.tsx
+++ app/page.tsx
@@ -28,13 +28,13 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        chronark
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-cyan cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        TranMuse
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
+        <h2 className="text-sm text-blue-500">
           I'm building{" "}
           <Link
             target="_blank"
